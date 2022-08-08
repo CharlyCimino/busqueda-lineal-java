5fd29e035096e79861870851604552846c0320fc
Charly Cimino <carlosecimino@gmail.com>
2022-08-08 20:00:59 -0300

    Paquete

--- src/ar/charlycimino/ejemplos/busquedalineal/Concesionaria.java
+++ src/ar/charlycimino/ejemplos/busquedalineal/Concesionaria.java
@@ -1,4 +1,4 @@
-package busquedalineal;
+package ar.charlycimino.ejemplos.busquedalineal;
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- src/ar/charlycimino/ejemplos/busquedalineal/Principal.java
+++ src/ar/charlycimino/ejemplos/busquedalineal/Principal.java
@@ -1,4 +1,4 @@
-package busquedalineal;
+package ar.charlycimino.ejemplos.busquedalineal;
 
 /**
  *
